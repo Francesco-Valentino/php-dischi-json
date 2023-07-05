e656d35b9fdfbb1269c5292b2d70284ff9d22deb
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-07-05 16:55:08 +0200

    set api url and api get call

--- js/main.js
+++ js/main.js
@@ -3,12 +3,19 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-
+            apiURL: './server.php',
         }
     },
 
     methods: {
-        
+        getAlbum(){
+            axios.get(this.apiURL).then((response)=>    {
+                console.log(response.data);
+            })
+        },
     },
 
+    created() {
+      this.getAlbum();  
+    },
 }).mount('#app');
